182ccaa893e360cd7aa9f398677ad2d9b6c53df3
minsub jang <msjang@okpos.co.kr>
2023-08-25 17:32:48 +0900

    first commit

---
+++ src/main/kotlin/com/ms/totfinan/TotfinanApplication.kt
@@ -0,0 +1,11 @@
+package com.ms.totfinan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TotfinanApplication
+
+fun main(args: Array<String>) {
+	runApplication<TotfinanApplication>(*args)
+}
---
+++ src/test/kotlin/com/ms/totfinan/TotfinanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ms.totfinan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TotfinanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
